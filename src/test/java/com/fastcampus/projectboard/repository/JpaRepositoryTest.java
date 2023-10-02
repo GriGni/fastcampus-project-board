678e033d5c3ada6c6b5fea7e1716e76f54c90133
geun <geun@howoocast.com>
2023-10-02 12:03:25 +0900

    feat : jpa test 작성 * 기본 crud + 연관 관계 매핑과 cascading이 잘 동작하는지 확인하려고 작성 테스트 데이터와 관련이 있기 때문에 이대로는 실패한다.

---
+++ src/test/java/com/fastcampus/projectboard/repository/JpaRepositoryTest.java
@@ -0,0 +1,89 @@
+package com.fastcampus.projectboard.repository;
+
+import com.fastcampus.projectboard.config.JpaConfig;
+import com.fastcampus.projectboard.domain.Article;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+// jpa config 를 모름 그래서 import 추가해줘야 함
+@DisplayName("JPA 연결 테스트")
+@Import(JpaConfig.class)
+@ActiveProfiles("fastcampus_test_db")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@DataJpaTest
+class JpaRepositoryTest {
+
+    private final ArticleRepository articleRepository;
+    private final ArticleCommentRepository articleCommentRepository;
+
+    public JpaRepositoryTest(@Autowired ArticleRepository articleRepository,
+                             @Autowired ArticleCommentRepository articleCommentRepository
+    ) {
+        this.articleRepository = articleRepository;
+        this.articleCommentRepository = articleCommentRepository;
+    }
+
+    @DisplayName("select test")
+    @Test
+    void givenTestData_whenSelecting_thenWorksFine(){
+        //given
+
+        //when
+        List<Article> articles = articleRepository.findAll();
+        //then
+        assertThat(articles).isNotNull().hasSize(10);
+    }
+
+    @DisplayName("insert test")
+    @Test
+    void givenTestData_whenInserting_thenWorksFine(){
+        //given
+        long previousCount = articleRepository.count();
+        Article article = Article.of("new article", "new content", "#spring");
+        //when
+        Article savedArticle = articleRepository.save(article);
+        //then
+        assertThat(articleRepository.count()).isEqualTo(previousCount + 1);
+    }
+
+    @DisplayName("update test")
+    @Test
+    void givenTestData_whenUpdating_thenWorksFine(){
+        //given
+        Article article = articleRepository.findById(1L).orElseThrow();
+        String updatedHashtag = "#spring-boot";
+        article.setHashtag(updatedHashtag);
+        //when
+        Article savedArticle = articleRepository.saveAndFlush(article);
+        //then
+        assertThat(savedArticle).hasFieldOrPropertyWithValue("hashtag", updatedHashtag);
+    }
+
+    @DisplayName("delete test")
+    @Test
+    void givenTestData_whenDeleting_thenWorksFine(){
+        //given
+        Article article = articleRepository.findById(1L).orElseThrow();
+        long previousArticleCount = articleRepository.count();
+        long previousArticleCommentCount = articleCommentRepository.count();
+        int deletedCommentsSize = article.getArticleComments().size();
+
+        //when
+        articleRepository.delete(article);
+
+        //then
+        assertThat(articleRepository.count()).isEqualTo(previousArticleCount - 1);
+        assertThat(articleCommentRepository.count()).isEqualTo(previousArticleCommentCount - deletedCommentsSize);
+    }
+
+}
